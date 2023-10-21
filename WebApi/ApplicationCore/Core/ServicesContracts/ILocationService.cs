73a1d2e74eae015bbfc24fdfc713fc558202049e
pc2x <alberto_ville@hotmail.com>
2023-10-21 16:16:04 -0600

    [JAVN add webApi]

---
+++ WebApi/ApplicationCore/Core/ServicesContracts/ILocationService.cs
@@ -0,0 +1,9 @@
+﻿using WebApiNetCore2023.ApplicationCore.Core.Models;
+
+namespace WebApiNetCore2023.ApplicationCore.Core.ServicesContracts
+{
+    public interface ILocationService : IGenericService<LocationModel,string>
+    {
+
+    }
+}
---
+++ WebApi/Identity/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApiNetCore2023.Models.Identity
+{
+    public class ApplicationDbContext: IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
